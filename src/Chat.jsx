cfbcc41fde73aa8183a81de538b104308d5df3b3
larans <larans7277@gmail.com>
2023-08-11 22:17:40 +0900

    footer remove

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -6,9 +6,11 @@ import Footer from "./assets/footer.png"
 
 function Chat() {
   useEffect(() => {
-    const element = document.getElementById('lite-badge');
-    
-    console.log('ddddddddd', element);
+    const elements = document.getElementsByClassName('object-cover');
+    console.log("ele",elements);
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].style.visibility = 'hidden';
+    }
     
   }, []);
   return (
@@ -16,7 +18,7 @@ function Chat() {
         <div className="w-screen h-[5%] flex bg-emerald-800 items-center">
           <a href="/astrologia">
             <svg className="w-12 h-12 py-2 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="back-arrow"><g data-name="Layer 2"><g data-name="arrow-back"><rect width="24" height="24" opacity="0" transform="rotate(90 12 12)"></rect><path d="M19 11H7.14l3.63-4.36a1 1 0 1 0-1.54-1.28l-5 6a1.19 1.19 0 0 0-.09.15c0 .05 0 .08-.07.13A1 1 0 0 0 4 12a1 1 0 0 0 .07.36c0 .05 0 .08.07.13a1.19 1.19 0 0 0 .09.15l5 6A1 1 0 0 0 10 19a1 1 0 0 0 .64-.23 1 1 0 0 0 .13-1.41L7.14 13H19a1 1 0 0 0 0-2z"></path></g></g></svg>
-          </a><img src={Avatar} className="w-9 h-9 mr-2" alt="avatar"/>
+          </a><img src={Avatar} className="w-9 h-9 mr-2 myLink" alt="avatar" id="myLink"/>
           <div className=" text-white">
             <div className="font-semibold text-lg">Nadja Marseille</div>
             <div className="text-base text-left -mt-1">online</div>
@@ -31,8 +33,7 @@ function Chat() {
           typebot="astrologia-9azb8xn"
           className="w-full h-[95%]"
         />
-        <img src={Footer} alt='footer' className=" absolute w-full h-[50px] bottom-0 z-50"/> 
-
+        {/* <img src={Footer} alt='footer' className=" absolute w-full h-[50px] bottom-0 z-50 md:h-[50px] sm:bottom-0 sm:h-[50px] md:bottom-0 md:z-50 sm:z-50"/>  */}
         
     </div>
   );
